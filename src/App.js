adbce8db14a2cee8c9f6cf0fbc86864b6ca1c66c
Dhruv Arora <dhruvarora.code@gmail.com>
2024-01-24 19:05:15 +0530

    implemented to-do app structure and design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+const initialTasks = [
+  {
+    task: "brush teeth",
+    number: 1,
+  },
+  {
+    task: "drink milk",
+    number: 1,
+  },
+  {
+    task: "buy bread",
+    number: 1,
+  },
+  {
+    task: "go shopping",
+    number: 1,
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <Heading />
+      <InputForm />
+      <TaskList />
     </div>
   );
 }
 
-export default App;
+function Heading() {
+  return <h1>Get Things Done!</h1>;
+}
+
+function InputForm() {
+  return (
+    <form>
+      <input className="input" type="text" placeholder="Type here..." />
+      <button>Add Task</button>
+    </form>
+  );
+}
+
+function TaskList() {
+  return (
+    <div className="tasklist">
+      {initialTasks.map((task) => (
+        <Task task={task} key={task.number} />
+      ))}
+    </div>
+  );
+}
+
+function Task({ task }) {
+  return (
+    <p className="task">
+      {task.task}
+      <span>🖋️ ❌</span>
+    </p>
+  );
+}
